f2c598c9817eee198d5528ba151b738ed14c70fa
Hasan <118883897+HasanMorovvati2004@users.noreply.github.com>
2023-08-21 09:27:54 +0330

    Add files via upload

---
+++ game-level.py
@@ -0,0 +1,25 @@
+import random
+while True:
+    try:
+        x=int(input("Level: "))
+        if x >0 :
+            break
+    except:
+        pass
+
+y = random.randint(1, x)
+while True:
+    try:
+        z=int(input("Guess: "))
+        if z>0 :
+            if z < y:
+                print("Too small!")
+            elif z > y:
+                print("Too large!")
+            else:
+                print("Just right!")
+                break
+    except:
+        pass
+
+
